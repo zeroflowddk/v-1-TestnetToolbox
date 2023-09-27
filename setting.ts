3d778b56d00ff3ff046e465afdd567dd11aabec9
Oʀɪɢᴀᴍɪ <aoyamalinux@gmail.com>
2023-09-27 15:25:33 +0300

    updated

--- setting.ts
+++ setting.ts
@@ -1,12 +1,16 @@
 export class Taiko {
     static amountProcentIn = 20;
     static amountProcentOut = 20;
-    static amountSwap = 20;
+    
 };
 
 export class Scroll {
     static amountProcentIn = 30;
-    static amountSwap = 20;
+};
+
+export class Manta {
+    static amountProcentIn = 30;
+    static amountProcentOut = 20;
 };
 
 
